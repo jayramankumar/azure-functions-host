ac7ee4208aa8876defcb7cc20a4ec936f921d5a8
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2018-07-17 15:08:22 -0700

    Handling host intialization. Introduced a function metadata service

--- src/WebJobs.Script.WebHost/Management/WebFunctionsManager.cs
+++ src/WebJobs.Script.WebHost/Management/WebFunctionsManager.cs
@@ -131,7 +131,9 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Management
         /// <returns>(success, FunctionMetadataResponse)</returns>
         public async Task<(bool, FunctionMetadataResponse)> TryGetFunction(string name, HttpRequest request)
         {
-            var functionMetadata = ScriptHost.ReadFunctionMetadata(Path.Combine(_config.RootScriptPath, name), new Dictionary<string, Collection<string>>(), fileSystem: FileUtility.Instance);
+            // TODO: DI (FACAVAL) Follow up with ahmels - Since loading of function metadata is no longer tied to the script host, we
+            // should be able to inject an IFunctionMedatadaManager here and bypass this step.
+            var functionMetadata = FunctionMetadataManager.ReadFunctionMetadata(Path.Combine(_config.RootScriptPath, name), null, new Dictionary<string, Collection<string>>(), fileSystem: FileUtility.Instance);
             if (functionMetadata != null)
             {
                 return (true, await functionMetadata.ToFunctionMetadataResponse(request, _config));
@@ -244,8 +246,8 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Management
 
         private IEnumerable<FunctionMetadata> GetFunctionsMetadata()
         {
-            return ScriptHost
-                .ReadFunctionsMetadata(FileUtility.EnumerateDirectories(_config.RootScriptPath), _logger, new Dictionary<string, Collection<string>>(), fileSystem: FileUtility.Instance);
+            return FunctionMetadataManager
+                .ReadFunctionsMetadata(FileUtility.EnumerateDirectories(_config.RootScriptPath), null, _logger, fileSystem: FileUtility.Instance);
         }
 
         private async Task<Dictionary<string, string>> ReadDurableTaskConfig()
